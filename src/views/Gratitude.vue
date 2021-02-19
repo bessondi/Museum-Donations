c05cd4252c58c15abcd3df0a56f03880fd359621
Dmitry Bessonov <dmitrybessonov@icloud.com>
2021-02-19 14:45:24 +0300

    add html-to-img saver for gratitude page feature

--- src/views/Gratitude.vue
+++ src/views/Gratitude.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="gratitude">
+<div class="wrap">
+
+  <div class="gratitude" ref="gratitudePage">
     <div class="museumLogo">
       <img class="logo" :src="$store.state.locale === 'ru' ? fabergeLogos.ru : fabergeLogos.en" alt="Museum logo">
     </div>
 
-    <h1>
+    <h1 class="gratitude__heading">
       {{
         $store.state.nameValue.length || $store.state.surnameValue.length
           ? $store.state.nameValue + ' ' + $store.state.surnameValue
           : $locale('form.gratitudeDefaultText')
       }}
     </h1>
-    <p>{{ $locale('form.gratitudeText') }}</p>
+    <p class="gratitude__description">{{ $locale('form.gratitudeText') }}</p>
 
-    <img :src="image" alt="Museum image" class="image">
+    <img :src="image" alt="Museum image" class="gratitude__image">
   </div>
+
+  <button @click="saveAsImage" class="saveImgBtn">Сохранить</button>
+</div>
 </template>
 
 <script>
-import fabergeLogoRu from "@/assets/svg/faberge_logo_central_rus.svg"
-import fabergeLogoEn from "@/assets/svg/faberge_logo_central_eng.svg"
+import html2canvas from "html2canvas"
+import fabergeLogoRu from "@/assets/img/faberge_logo_central_rus.png"
+import fabergeLogoEn from "@/assets/img/faberge_logo_central_eng.png"
 import image from "@/assets/img/BlueRoom.jpg"
 
 export default {
@@ -30,6 +36,20 @@ export default {
         en: fabergeLogoEn,
       },
       image
+    }
+  },
+  methods: {
+    saveAsImage() {
+      window.scrollTo(0, 0)
+
+      html2canvas(this.$refs.gratitudePage)
+        .then(canvas => {
+          const link = document.createElement('a');
+          link.download = 'Faberge gratitude.jpg';
+          link.href = canvas.toDataURL('image/jpeg').replace("image/jpeg", "image/octet-stream")
+          link.click();
+          link.delete;
+        })
     }
   }
 }
