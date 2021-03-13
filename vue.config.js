52fdc65430ac3630e1e1d0f5681ac9e1a40aab08
Dmitry Bessonov <dmitrybessonov@icloud.com>
2021-03-13 13:21:56 +0300

    update available amount sum

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   lintOnSave: false,
 
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = "Fabergé Museum Donations";
+        return args;
+      })
+  }
+
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/Museum-Donations/'
   //   : '/'
